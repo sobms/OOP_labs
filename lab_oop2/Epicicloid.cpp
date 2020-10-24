d4fbc16260c6461661039ddc122ab29c032e3a54
sobms2020 <62150448+sobms2020@users.noreply.github.com>
2020-10-24 16:59:58 +0300

    lab oop2 remade

--- lab_oop2/Epicicloid.cpp
+++ lab_oop2/Epicicloid.cpp
@@ -1,23 +1,37 @@
+#define _USE_MATH_DEFINES
 #include "pch.h"
 #include "Epicicloid.h"
 #include <iostream>
 #include <math.h>
 #include <cmath>
+#define M_PI 3.141592653589793
 
 namespace epicicloid1 {
 	// constructors
 	Epicicloid::Epicicloid() : r(1.0), R(1.0), d(1.0), p() {}
 	Epicicloid::Epicicloid(const point& p0, double r0, double R0, double d0) : p(p0) {
-		if ((R0 < 0) || ((-1) * d0 > R0) || ((-1) * r0 > R0)) {
-			throw std::exception("invalid value");
+		if (R0 < 0) { // разделить exeption, сделать для каждого условия+
+			throw std::exception("the negative value of radius R");
+		}
+		if ((-1) * d0 > R0) {
+			throw std::exception("the negative value of d more then radius R");
+		}
+		if ((-1) * r0 > R0) {
+			throw std::exception("the negative value of r more then radius R");
 		}
 		this->R = R0;
 		this->d = d0;
 		this->r = r0;
 	}
 	Epicicloid::Epicicloid(double x0, double y0, double r0, double R0, double d0) : p(x0, y0) {
-		if ((R0 < 0) || ((-1) * d0 > R0) || ((-1) * r0 > R0)) {
-			throw std::exception("invalid value");
+		if (R0 < 0) {
+			throw std::exception("the negative value of radius R");
+		}
+		if ((-1) * r0 > R0) {
+			throw std::exception("the negative value of r more then radius R");
+		}
+		if ((-1) * d0 > R0)  {
+			throw std::exception("the negative value of d more then radius R");
 		}
 		this->R = R0;
 		this->d = d0;
@@ -26,45 +40,51 @@ namespace epicicloid1 {
 	//set'теры
 	Epicicloid& Epicicloid::set_r(double r0) {
 		if ((-1) * r0 > R) {
-			throw std::exception("invalid error");
+			throw std::exception("the negative value of r more then radius R");
 		}
 		r = r0;
 		return *this;
 	}
 	Epicicloid& Epicicloid::set_d(double d0) {
 		if ((-1) * d0 > R) {
-			throw std::exception("invalid error");
+			throw std::exception("the negative value of d more then radius R");
 		}
 		d = d0;
 		return *this;
 	}
 	Epicicloid& Epicicloid::set_R(double R0) {
 		if (R0 < 0) {
-			throw std::exception("invalid value");
+			throw std::exception("the negative value of radius R");
 		}
 		R = R0;
 		return *this;
 	}
 	point Epicicloid::get_coordinates(double angle) const {
 		point p0;
-		p0.x = p.x + (R + r) * cos(angle) - d * cos((R + r) * angle / r);
-		p0.y = p.y + (R + r) * sin(angle) - d * sin((R + r) * angle / r);
+		const double angle_rad = angle / 180 * M_PI;
+		p0.x = p.x + (R + r) * cos(angle_rad) - d * cos((R + r) * angle_rad / r);
+		p0.y = p.y + (R + r) * sin(angle_rad) - d * sin((R + r) * angle_rad / r);
 		return p0;
 	}
 	double Epicicloid::get_curv_rad(double angle) const {
-		double angle_rad = angle / 180 * 3.14159;
+		const double angle_rad = angle / 180 * M_PI;         // использовать константу+
 		return (R + r) * pow((pow(r, 2) + pow(d, 2) - 2 * d * r * cos(R * angle_rad / r)), 1.5) / abs(pow(r, 3) + pow(d, 2) * (R + r) - d * r * (R + 2 * r) * cos(R * angle_rad / r));
 	}
-	const char* Epicicloid::get_type() const {
-		const char* type;
+	int Epicicloid::get_type() const { //использовать enum предпочтительнее+
+		enum Epicicloid_type {
+			Shortened = 1,
+			Extended,
+			Ordinary
+		};
+		int type;
 		if (d < r) {
-			type = "shortened\0";
+			type = Shortened;
 		}
 		else if (d > r) {
-			type = "extended\0";
+			type = Extended;
 		}
 		else {
-			type = "ordinary\0";
+			type = Ordinary;
 		}
 		return type;
 	}
@@ -73,6 +93,6 @@ namespace epicicloid1 {
 		return ((R + r) / 2) * ((R + r + pow(d, 2) / r) * angle_rad - d * ((R + 2 * r) / R) * sin(R * angle_rad / r));
 	}
 	bool Epicicloid::Is_astroid() const {
-		return ((r < 0) && (abs(R) == 4 * abs(r)) && (r == d)) ? true : false;
+		return ((r < 0) && (abs(R) == 4 * abs(r)) && (r == d));
 	}
 }
--- lab_oop2/test.cpp
+++ lab_oop2/test.cpp
@@ -6,61 +6,95 @@
 namespace epc = epicicloid1;
 TEST(EpicicloidConstrucor, EmptyConstructor) {
 	epc:: Epicicloid e1;
-	ASSERT_EQ(1.0, e1.get_r());
-	ASSERT_EQ(1.0, e1.get_R());
-	ASSERT_EQ(1.0, e1.get_d());
-	ASSERT_EQ(0.0, e1.get_p().x);
-	ASSERT_EQ(0.0, e1.get_p().y);
+	double approx_val = 0.00001;
+	ASSERT_NEAR(1.0, e1.get_r(), approx_val);     // assert near для вещественных значений+
+	ASSERT_NEAR(1.0, e1.get_R(), approx_val);
+	ASSERT_NEAR(1.0, e1.get_d(), approx_val);
+	ASSERT_NEAR(0.0, e1.get_p().x, approx_val);
+	ASSERT_NEAR(0.0, e1.get_p().y, approx_val);
 }
 TEST(EpicicloidConstructor, InitConstructor) {
 	epc::point p (-1, 2);
+	double approx_val = 0.00001;
 	epc::Epicicloid e1(p, 3, 15, 5);
-	ASSERT_EQ(-1.0, e1.get_p().x);
-	ASSERT_EQ(2.0, e1.get_p().y);
-	ASSERT_EQ(3.0, e1.get_r());
-	ASSERT_EQ(15.0, e1.get_R());
-	ASSERT_EQ(5.0, e1.get_d());
+	ASSERT_NEAR(-1.0, e1.get_p().x, approx_val);
+	ASSERT_NEAR(2.0, e1.get_p().y, approx_val);
+	ASSERT_NEAR(3.0, e1.get_r(), approx_val);
+	ASSERT_NEAR(15.0, e1.get_R(), approx_val);
+	ASSERT_NEAR(5.0, e1.get_d(), approx_val);
 }
 TEST(EpicicloidConstructor, InitConstructor2) {
 	epc::Epicicloid e1(-3,-4, 3, 15, 5);
-	ASSERT_EQ(-3.0, e1.get_p().x);
-	ASSERT_EQ(-4.0, e1.get_p().y);
-	ASSERT_EQ(3.0, e1.get_r());
-	ASSERT_EQ(15.0, e1.get_R());
-	ASSERT_EQ(5.0, e1.get_d());
+	double approx_val = 0.00001;
+	ASSERT_NEAR(-3.0, e1.get_p().x, approx_val);
+	ASSERT_NEAR(-4.0, e1.get_p().y, approx_val);
+	ASSERT_NEAR(3.0, e1.get_r(), approx_val);
+	ASSERT_NEAR(15.0, e1.get_R(), approx_val);
+	ASSERT_NEAR(5.0, e1.get_d(), approx_val);
 }
 TEST(EpicicloidConstructor, Exception) {
 	
-	ASSERT_ANY_THROW(epc::Epicicloid e1(0, 0, 4, -5, 4));
-	ASSERT_ANY_THROW(epc::Epicicloid e1(0, 0, -16, 10, -5));
-	ASSERT_ANY_THROW(epc::Epicicloid e1(0, 0, 4, 12, -20));
+	ASSERT_THROW(epc::Epicicloid e1(0, 0, 4, -5, 4), std::exception); // проверить ожидаемый тип исключения +!???? сообщение как проверить
+	ASSERT_THROW(epc::Epicicloid e1(0, 0, -16, 10, -5), std::exception);
+	ASSERT_THROW(epc::Epicicloid e1(0, 0, 4, 12, -20), std::exception);
 }
 TEST(EpicicloidMethods, Setters) {
 	epc::Epicicloid a;
+	double approx_val = 0.00001;
 	a.set_r(6);
-	ASSERT_EQ(6, a.get_r());
+	ASSERT_NEAR(6.0, a.get_r(), approx_val);// проверить что другие значения не изменились+
+	ASSERT_NEAR(1.0, a.get_R(), approx_val);
+	ASSERT_NEAR(1.0, a.get_d(), approx_val);
+	ASSERT_NEAR(0.0, a.get_p().x, approx_val);
+	ASSERT_NEAR(0.0, a.get_p().y, approx_val);
+
 	a.set_R(5);
-	ASSERT_EQ(5, a.get_R());
+	ASSERT_NEAR(6.0, a.get_r(), approx_val);
+	ASSERT_NEAR(5.0, a.get_R(), approx_val);
+	ASSERT_NEAR(1.0, a.get_d(), approx_val);
+	ASSERT_NEAR(0.0, a.get_p().x, approx_val);
+	ASSERT_NEAR(0.0, a.get_p().y, approx_val);
+
 	a.set_d(4);
-	ASSERT_EQ(4, a.get_d());
+	ASSERT_NEAR(6.0, a.get_r(), approx_val);
+	ASSERT_NEAR(5.0, a.get_R(), approx_val);
+	ASSERT_NEAR(4.0, a.get_d(), approx_val);
+	ASSERT_NEAR(0.0, a.get_p().x, approx_val);
+	ASSERT_NEAR(0.0, a.get_p().y, approx_val);
+
 	a.set_point({ 8.0, 8.0 });
-	ASSERT_EQ(8, a.get_p().x);
-	ASSERT_EQ(8, a.get_p().y);
-	ASSERT_ANY_THROW(a.set_r(-6));
-	ASSERT_ANY_THROW(a.set_d(-7));
-	ASSERT_ANY_THROW(a.set_R(-1));
+	ASSERT_NEAR(6.0, a.get_r(), approx_val);
+	ASSERT_NEAR(5.0, a.get_R(), approx_val);
+	ASSERT_NEAR(4.0, a.get_d(), approx_val);
+	ASSERT_NEAR(8.0, a.get_p().x, approx_val);
+	ASSERT_NEAR(8.0, a.get_p().y, approx_val);
+
+	ASSERT_THROW(a.set_r(-6), std::exception);
+	ASSERT_THROW(a.set_d(-7), std::exception);
+	ASSERT_THROW(a.set_R(-1), std::exception);
 }
-TEST(EpicicloidMethods, Selectors) {
-	epc::Epicicloid a(0, 0, 2, 5, 3); 
+TEST(EpicicloidMethods, Selectors) { // дополнительные тесты с другими значениями+
+	epc::Epicicloid a(0, 0, 2, 5, 3);
 	double error = (0.05);
-	ASSERT_NEAR(a.get_coordinates(60).x, -4.015, error);
-	ASSERT_NEAR(a.get_coordinates(60).y, -3.537, error);
+	ASSERT_NEAR(a.get_coordinates(60).x, 6.098, error);
+	ASSERT_NEAR(a.get_coordinates(60).y, 7.562178, error);
 	ASSERT_EQ(10, a.get_border_rads().R);
 	ASSERT_EQ(4, a.get_border_rads().r);
 	ASSERT_EQ(false, a.Is_astroid());
-	ASSERT_EQ("extended\0", a.get_type());
+	ASSERT_EQ(2, a.get_type());
 	ASSERT_NEAR(a.sect_area(60), 32.7, error);
 	ASSERT_NEAR(a.get_curv_rad(60), 6.72, 0.01);
+}
+
+TEST(EpicicloidMethods2, Selectors) {
+	epc::Epicicloid a(4, 2, -2, 8, -2);
+	double error = (0.05);
+	ASSERT_NEAR(a.get_coordinates(30).y, 3.0, error);
+	ASSERT_NEAR(a.get_coordinates(30).x, 9.19615, error);
+	ASSERT_EQ(true, a.Is_astroid());
+	ASSERT_EQ(3, a.get_type());
+	ASSERT_NEAR(a.sect_area(30), 3.6851, error);
+	ASSERT_NEAR(a.get_curv_rad(30), 10.3923, 0.01);
 }
 
 
